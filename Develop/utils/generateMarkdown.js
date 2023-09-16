c4df510fdc70c7629ba99e934e2117f5d092a8fa
ChgDave <chgdave@gmail.com>
2023-09-16 14:56:07 -0500

    my code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge;
+  //only 4 license options switch statement
+  switch (license) {
+    case "The MIT License":
+      licenseBadge = "https://img.shields.io/badge/License-MIT-yellow.svg";
+      break;
+    case "Apache 2.0 License":
+      licenseBadge = "https://img.shields.io/badge/License-Apache_2.0-blue.svg";
+      break;
+    case "Eclipse Public License 1.0":
+      licenseBadge = "https://img.shields.io/badge/License-EPL_1.0-red.svg";
+      break;
+    case "Mozilla Public License 2.0":
+      licenseBadge =
+        "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg";
+      break;
+  }
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // only 4 license options
+  let licenseLink;
+  //only 4 license options switch statement
+  switch (license) {
+    case "The MIT License":
+      licenseLink = "https://opensource.org/licenses/MIT";
+      break;
+    case "Apache 2.0 License":
+      licenseLink = "https://opensource.org/licenses/Apache-2.0";
+      break;
+    case "Eclipse Public License 1.0":
+      licenseLink = "https://opensource.org/licenses/EPL-1.0";
+      break;
+    case "Mozilla Public License 2.0":
+      licenseLink = "https://opensource.org/licenses/MPL-2.0";
+      break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
